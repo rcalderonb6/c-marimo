59f76d2447ce44f7e79bb5dd2057b29271a3fb6d
Rodrigo Calderon <rodrigocalderon273@gmail.com>
2025-04-06 14:23:35 +0200

    minor changes

--- README.md
+++ README.md
@@ -14,4 +14,4 @@
 
 ## Features
 
--   TODO
+
---
+++ c_marimo/cli.py
@@ -0,0 +1,4 @@
+def main():
+    print("Hello, Marimo!")
+    print("This is the CLI for Marimo.")
+    print("You can use this to interact with the Marimo library.")
